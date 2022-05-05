afdef2f67a358277cc2061c7e766e7c3207fd700
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-05-06 00:48:50 +0200

    fix: edit sizes correspondently

--- src/index.js
+++ src/index.js
@@ -22,14 +22,14 @@ const firstRus = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '
 const secRus = ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'];
 const thiRus = ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'];
 const fourRus = ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Top', 'Shift'];
-const fifRus = ['Ctrl', 'Win', 'Alt', 'space', 'Alt', 'left', 'bottom', 'Ctrl'];
+const fifRus = ['Ctrl', 'Win', 'Alt', 'space', 'Alt', 'left', 'bottom', 'right', 'Ctrl'];
 
 function create(arr, row) {
   let regButt;
   for (let i = 0; i < arr.length; i++) {
     regButt = document.createElement('div');
     regButt.classList.add('regButt');
-    regButt.setAttribute('id', `${arr[i]}`);
+    regButt.setAttribute('id', `${arr[i] + [i]}`);
     row.appendChild(regButt);
     regButt.innerText = arr[i];
   }
@@ -38,7 +38,6 @@ function create(arr, row) {
 
 create(firstRus, firstRow);
 
-
 create(secRus, secRow);
 
 create(thiRus, thiRow);
@@ -46,3 +45,8 @@ create(thiRus, thiRow);
 create(fourRus, fourRow);
 
 create(fifRus, fifRow);
+
+const shift = document.getElementById('Shift_2');
+const shiftText = shift.innerText;
+shiftText = 'Shift';
+shiftText.style.color = 'black';
